1101465889115abbc5ba1885a7be21d9a1e6df22
MykolaDekusar <ndekusar97@gmail.com>
2024-10-27 14:36:13 +0100

    Initialization of array with assignation of values, for cycle on array with print of all values using i as index

--- JavaArrays/src/Main.java
+++ JavaArrays/src/Main.java
@@ -2,6 +2,15 @@
 public class Main {
 
 	public static void main(String[] args) {
+		//un Array è una variabile che serve a contenere il valore di più variabili
+		//inizializzo l'array con String[] e gli asseno i valori
+		String[] sonoArray = {"sono il primo valore", "sono il secondo valore", "sono il tezo valore"};
+		//l'array parte da 0 e per prendere il valore dobbiamo prendere l'array e inserire il numero dell'indice
+		//del valore che stiamo cercando di ottenere
+		//ciclo sulla lunghezza dell'array e stampo tutti i valori
+		for(int i=0; i<sonoArray.length;i++) {
+			System.out.println(sonoArray[i]);
+		}
 		
 	}
 
